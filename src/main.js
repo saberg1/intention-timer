2455e2961d6f348b58bbfb10ae3c9045b8c22c62
William McGuire <wmcguire18@gmail.com>
2021-04-13 16:45:58 -0400

    Clean up html

--- src/main.js
+++ src/main.js
@@ -1,30 +1,24 @@
 //Query Selectors
-var acitivyInput = document.querySelector('#activityDescription');
+var activityInput = document.querySelector('#activityDescription');
 var minutesInput = document.querySelector('#minutes');
 var secondsInput = document.querySelector('#seconds');
 
 var studyImg = document.querySelector('#study');
-var meditatImg = document.querySelector('#meditate')
-var exerciseImg = document.querySelector('#exercise')
+var meditateImg = document.querySelector('#meditate');
+var exerciseImg = document.querySelector('#exercise');
 
 var submitBtn = document.querySelector('#submit');
 
 var imageSection = document.querySelector('#imageSection');
 var form = document.querySelector('#form');
-var rightAside = document.querySelector('#rightAside')
+var rightAside = document.querySelector('#rightAside');
 
 //Event Listeners
-
+studyImg.addEventListener("click", changeColor);
+// meditateImg.addEventListener('click');
+// exerciseImg.addEventListener('click');
 
 //Event Handlers
 function changeColor(){
-  if(event.target.id === 'study'){
-    console.log('study')
-  }
-  if(event.target.id === 'meditate'){
-    console.log('mediate')
-  }
-  if(event.target.id === 'exercise'){
-    console.log('exercise')
-  }
-}
+studyImg.innerHTML = `<img src="./assets/study-active.svg" alt="study-active"/>`
+};
