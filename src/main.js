82a137b2043f259eb380847c37eb998b7720b719
William McGuire <wmcguire18@gmail.com>
2021-04-14 20:59:03 -0400

    Add further color functionality

--- src/main.js
+++ src/main.js
@@ -44,19 +44,22 @@ function changeMe(){
     meditateImg.src = "./assets/meditate.svg"
     exerciseImg.src = "./assets/exercise.svg"
     studyRadio.checked ? studyImg.src = "./assets/study-active.svg" : studyImg.src = "./assets/study.svg"
-    studyContainer.style.border = "2px #B3FD78 solid"
+    // studyContainer.style.border = "2px #B3FD78 solid"
+    studyContainer.classList.toggle('study')
   }
   if(meditateRadio.checked){
     exerciseImg.src = "./assets/exercise.svg"
     studyImg.src = "./assets/study.svg"
     meditateRadio.checked ? meditateImg.src = "./assets/meditate-active.svg" : meditateImg.src = "./assets/meditate.svg"
-    meditateContainer.style.border = "2px #C278FD solid"
+    // meditateContainer.style.border = "2px #C278FD solid"
+    meditateContainer.classList.toggle('meditate')
   }
   if(exerciseRadio.checked){
     studyImg.src = "./assets/study.svg"
     meditateImg.src = "./assets/meditate.svg"
     exerciseRadio.checked ? exerciseImg.src = "./assets/exercise-active.svg" : exerciseImg.src = "./assets/exercise.svg"
-    exerciseContainer.style.border = "2px #FD8078 solid"
+    // exerciseContainer.style.border = "2px #FD8078 solid"
+    exerciseContainer.classList.toggle('exercise')
   }
 }
 function submitData(){
